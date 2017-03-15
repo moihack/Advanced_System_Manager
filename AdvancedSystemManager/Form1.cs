b3640497c0bc88d8017bcd16d29b85910300fa77
moihack <moihack.inside@gmail.com>
2017-03-15 21:10:07 +0200

    final git test

--- AdvancedSystemManager/Form1.cs
+++ AdvancedSystemManager/Form1.cs
@@ -19,6 +19,7 @@ namespace AdvancedSystemManager
         {
             SystemInfo sysinfo = new SystemInfo();
             MessageBox.Show(sysinfo.HOSTNAME);
+            MessageBox.Show(RegistryParser.ParseRegistry());
         }
     }
 }
---
+++ AdvancedSystemManager/RegistryParser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvancedSystemManager
+{
+    class RegistryParser
+    {
+        public static String ParseRegistry()
+        {
+            return "parsed!";
+        }
+    }
+}
